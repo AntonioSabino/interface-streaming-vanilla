214227a4fc2869f923e208ef38b9156558df786f
Antonio Sabino <aslimajr@outlook.com>
2022-04-06 12:24:52 -0300

    Iniciando a responsividade

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,16 +1,19 @@
-$('.owl-carousel').owlCarousel({
-  loop:true,
-  margin:10,
-  nav:true,
-  responsive:{
-      0:{
-          items:2
-      },
-      600:{
-          items:3
-      },
-      1000:{
-          items:5
-      }
-  }
-})
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  responsive: {
+    0: {
+      items: 2,
+    },
+    500: {
+      items: 3,
+    },
+    800: {
+      items: 4,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
